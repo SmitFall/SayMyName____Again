1e06f8cee22ab092267f6632fb8479cde8dbfe32
Smitfall <captainfalconrsmith@gmail.com>
2022-01-24 19:34:11 -0700

    Add project files.

---
+++ SayMyName____Again/SayMyName____Again.vb
@@ -0,0 +1,25 @@
+﻿'Fallon Smith
+'RCET0265
+'Spring 2022
+'Say My Name....Again
+'
+
+Option Explicit On
+Option Strict On
+Option Compare Text
+Module SayMyName____Again
+
+    Sub Main()
+        'set aside varibles
+        Dim userResponse As String
+
+        Console.WriteLine("Please Enter your name")
+
+        If userResponse = "Emily" Then
+            Console.WriteLine("Welcome back Emily")
+        End If
+
+        Console.Read()
+    End Sub
+
+End Module
